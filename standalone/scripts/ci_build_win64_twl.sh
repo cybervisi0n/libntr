be5e91f27c1358a3fe70732948ec80a1ee5edef3
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-09-19 16:09:32 -0500

    CI: Add TWL build

---
+++ standalone/scripts/ci_build_win64_twl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+meson setup build
+meson configure -Dbuild_target=win64 build
+meson configure -Dsdk_version_major=5 build
+meson configure -Dhardware_type=twl_hyb build
+meson configure -Dstandalone=true build
+meson compile -C build
